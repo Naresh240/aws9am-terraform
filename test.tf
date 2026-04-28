99b471bba57a81a489fa6beac135bd0c9ecf4bc6
Naresh240 <58024415+Naresh240@users.noreply.github.com>
2026-04-28 09:31:29 +0530

    Add security group rule for HTTP traffic

--- test.tf
+++ test.tf
@@ -33,6 +33,17 @@ resource "aws_security_group" "example_0" {
     security_groups  = []
     self             = false
     to_port          = 22
+  }
+  {
+    cidr_blocks      = ["0.0.0.0/0"]
+    description      = ""
+    from_port        = 80
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    protocol         = "tcp"
+    security_groups  = []
+    self             = false
+    to_port          = 80
   }]
   name                   = "allow_ssh"
   region                 = "us-east-1"
